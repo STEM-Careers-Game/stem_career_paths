e509642f4b7872e9ffb9640855da54d1e238c7c6
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-03-28 13:48:24 -0400

    Factor out redundancy

--- project/end/epilogue_text_generator.gd
+++ project/end/epilogue_text_generator.gd
@@ -2,15 +2,25 @@ class_name EpilogueTextGenerator extends RefCounted
 
 func generate_gap_year_text(_epilogue:Epilogue) -> Array[EpilogueText]:
 	return [
-		EpilogueText.create_paragraph("You take a"),
-		EpilogueText.create_main_point("gap year"),
-		EpilogueText.create_paragraph("to save some money and buy a car."),
+		_p("You take a"),
+		_h("gap year"),
+		_p("to save some money and buy a car."),
 	]
 
 
 func generate_community_college_text(_epilogue:Epilogue) -> Array[EpilogueText]:
 	return [
-		EpilogueText.create_paragraph("You attend"),
-		EpilogueText.create_main_point("Community College"),
-		EpilogueText.create_paragraph("so that you can learn independence, practice college-level study skills, and save tuition money.")
+		_p("You attend"),
+		_h("Community College"),
+		_p("so that you can learn independence, practice college-level study skills, and save tuition money.")
 	]
+
+
+# "p" as in "paragraph" from HTML.
+func _p(s:String) -> EpilogueText:
+	return EpilogueText.create_paragraph(s)
+
+
+# "h" as in "heading" from HTML.
+func _h(s:String) -> EpilogueText:
+	return EpilogueText.create_main_point(s)
